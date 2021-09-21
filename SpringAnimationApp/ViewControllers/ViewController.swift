82262a01ded023b1395de84c64811f4cabebdba5
Lysenko Oleg <moto.froloff@yandex.ru>
2021-09-21 21:52:26 +0300

    Initial Commit

---
+++ SpringAnimationApp/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpringAnimationApp
+//
+//  Created by Олег Лысенко on 21.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
